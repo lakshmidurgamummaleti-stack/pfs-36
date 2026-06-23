9dfdbfdc40072ebec282b392792aec15fae5e199
lakshmidurgamummaleti-stack <lakshmidurgamummaleti@gmail.com>
2026-06-23 14:01:59 +0530

    Add files via upload

---
+++ examples.py
@@ -0,0 +1,55 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+pin="correct"
+balance=5000
+pin=="correct" and balance
+5000
+pin="correct"
+balance=5000
+pin=="correct" and balance>=1000
+True
+pin=="correct" and balance>5000
+False
+cgpa=8.2
+backlogs=0
+cgpa>=7.0 and backlogs==0
+True
+>>> cgpa=9.2 and backlogs==1
+>>> cgpa==9.2 and backlogs>=1
+False
+>>> #job application
+>>> degree=B.tech
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    degree=B.tech
+NameError: name 'B' is not defined
+>>> degree="btech"
+>>> experiance=2
+>>> degree=="betech" and experiance>=1
+False
+>>> degree=="btech" and experiance>1
+True
+>>> atm=
+SyntaxError: invalid syntax
+>>> atm="yes"
+>>> account="money"
+>>> atm=="yes" or account=="money"
+True
+>>> #online shopping offer
+>>> coupon="yes"
+>>> coupon=="yes" or coupon=="special"
+True
+>>> coupon=="no" or coupon=="special"
+False
+>>> #college admission
+>>> qualification="Inter"
+>>> qualification>="Inter" or qualification=="degree"
+True
+>>> #software job eligibility
+>>> skill=
+SyntaxError: invalid syntax
+>>> 
+
+>>> skill="python"
+>>> skill=="python" or skill=="java"
+True
