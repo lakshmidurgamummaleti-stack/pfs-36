ce0a37e4fde5cb5883ef1e6bcfd226c9fa76f4da
lakshmidurgamummaleti-stack <lakshmidurgamummaleti@gmail.com>
2026-06-23 13:58:34 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,183 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=12
+b=20
+a+b
+32
+a-b
+-8
+a*b
+240
+a%b
+12
+a/b
+0.6
+a//b
+0
+a**b
+3833759992447475122176
+a+=b
+a+=b
+a += b
+#Assignment operator
+a==b
+False
+a+=b
+a
+92
+b
+20
+a-=b
+a
+72
+b
+20
+a*=b
+a
+1440
+b
+20
+a="code"
+b="gnan"
+a+b
+'codegnan'
+a-b
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    a-b
+TypeError: unsupported operand type(s) for -: 'str' and 'str'
+a*b
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    a*b
+TypeError: can't multiply sequence by non-int of type 'str'
+#compariosion operators
+a>b
+False
+a<b
+True
+a>=b
+False
+a<=b
+True
+x=True
+type(x)
+<class 'bool'>
+#above is the type indication
+%-remainder
+SyntaxError: invalid syntax
+#// quotient for int
+#/-quotitent for float
+10%2
+0
+10//2
+5
+10/3
+3.3333333333333335
+a=20
+b=30
+a%b
+20
+a//b
+0
+a/b
+0.6666666666666666
+0.6666666666666666
+0.6666666666666666
+a%=b
+a
+20
+a+=b
+a
+50
+a-=b
+a
+20
+a//=b
+a
+0
+a/=b
+a
+0.0
+a=10
+b=20
+a>=b
+False
+a<=b
+True
+a!=b
+True
+s="b"
+z="c"
+s!=z
+True
+s<z
+True
+s>z
+False
+20<20.5
+True
+20>20.5
+False
+20>20.5
+False
+20<=20.5
+True
+20>=20.5
+False
+>>> 20!=20.5
+True
+>>> 20==20.5
+False
+>>> 10<5>6
+False
+>>> 20>30<40
+False
+>>> age=18;name="honey"
+>>> age
+18
+>>> name
+'honey'
+>>> age>15 and name=="honey"
+True
+>>> ticket="yes";bike="no"
+>>> ticket=="yes" and bike=="yes"
+False
+>>> ticket=="yes" and bike=="no"
+True
+>>> atm="yes"
+>>> account="money"
+>>> atm=="yes" or account="money"
+SyntaxError: cannot assign to expression
+>>> hyd="bus";
+>>> hyd=="bus" or hyd=="train"
+True
+>>> test="scam"
+>>> test=="scam" or test=="genuine"
+True
+>>> not(false)
+Traceback (most recent call last):
+  File "<pyshell#91>", line 1, in <module>
+    not(false)
+NameError: name 'false' is not defined. Did you mean: 'False'?
+>>> not(True)
+False
+>>> not(False)
+True
+cgpa>=7.0
+backlogs=0
+cgpa==7.0 and baacklogs==0
+cgpa=8.2
+backlogs=0
+cgpa>=7.0 and backlogs==0
+pin="correct"
+balance=5000
+pin=="correct" and balance>=1000
+age=18
+name="honey"
+age>=15 and name=="honey
+
+
+
+
+
