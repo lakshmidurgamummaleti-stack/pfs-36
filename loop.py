0baad3bdaa1e9280ea3520eb28697453b19b0ccc
lakshmidurgamummaleti-stack <lakshmidurgamummaleti@gmail.com>
2026-06-23 14:03:10 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,132 @@
+"""x=[10,20,30,40,50,60]
+for i in x:
+    print(i)"""
+
+"""x=[10,20,30,40,50,60]
+for i in x:
+    if i%2==0:
+        print(i)"""
+
+"""x=[10,15,20,25,30,35]
+for i in range:
+    if i%2!=0:
+        print(i)
+    else:
+        print("even")"""
+
+"""x="srujana"
+for i in x:
+    print(i.upper())"""
+
+
+"""x="sru78977ja47875na"
+for i in x:
+    if(i.isalpha()):
+        print(i)"""
+
+
+"""x="sru78977ja47875na"
+for i in x:
+    if(i.isdigit()):
+        print(i)"""
+
+"""x="sru78977ja47875na"
+w=""
+n=""
+for i in x:
+    if(i.isdigit()):
+        n+=i
+    else:
+        w+=i
+print(n)
+print(w)"""
+
+
+
+"""x="sru78977ja47875na"
+w=""
+n=0
+for i in x:
+    if(i.isdigit()):
+        n+=int(i)
+    else:
+        w+=i
+print(n)
+print(w)"""
+
+
+
+"""d={"a":1,"b":2,"c":3}
+for i in d.items():
+    print(i)"""
+
+"""d={"a":100,"b":200,"c":300}
+for k,v in d.items():
+    print(k,v)"""
+
+"""d={"a":100,"b":200,"c":300}
+for k,v in d.items():
+    if v>150:
+        print(k)"""
+
+
+"""for i in range(10,0,-1):
+    print(i)"""
+
+"""x=[1,2,3,4,5,6,7]
+for i in range(len(x)):
+    print(i)"""
+
+"""x=[1,2,3,4,5,6,7]
+for i in range(len(x)):
+    print(x[i])"""
+    
+
+"""x=[1,2,3,4,5,6,7]
+for i in range(len(x)-1,-1,-1):
+    print(i)"""
+        
+    
+"""x=[1,2,3,4,5,6,7]
+for i in range(len(x)-1,-1,-1):
+    if i%2==0:
+        if x[i]%2==0:
+            print(x[i])"""
+            
+        
+    
+"""x=[1,2,3,4,5,6,7]
+for i in range(len(x)-1,-1,-1):
+    if i%2==0:
+        if x[i]%2!=0:
+            print(x[i])"""
+
+"""for i in range(1,20,1):
+    print(i)
+else:
+    print("completed")"""
+        
+"""for i in range(-10,0,-1):
+    print(i)"""
+
+"""for i in range(-12,-14,-2):
+    print(i)"""
+
+"""for i in range(-12, -27,-3):
+    print(i)"""
+
+"""for i in range(-4,-23,-5):
+    print(i)"""
+"""for i in range(-2,-7,-1):
+    print(i)"""
+"""for i in range(-1,-7,-1):
+    print(i)"""
+    
+    
+
+"""for i in range(1,101):
+    if i%5==0:
+        continue
+    else:
+        print(i)"""
+        
