b5cfebb7129038280c15b1c230895c965568877d
lakshmidurgamummaleti-stack <lakshmidurgamummaleti@gmail.com>
2026-06-23 14:04:44 +0530

    Add files via upload

---
+++ conditional st.py
@@ -0,0 +1,89 @@
+"""user="lakshmi"
+password=12345
+u=input()
+p=int(input())
+if u==user and p==password:
+    print("login")
+else:
+    print("wrong credential")"""
+
+""""a,b,c=map(int,input().split())
+if a>b and a>c:
+    print(a)
+elif b>a and b>c:
+    print(b)
+else:
+    print(c)"""
+    
+"""a,b,c=map(int,input().split())
+if a<b and a<c:
+    print(a)
+elif b<a and b<c:
+    print(b)
+else:
+    print(c)"""
+    
+    
+    
+"""a=int(input())
+if a%3==0:
+    print("fizz")
+elif a%5==0:
+    print("buzz")"""
+    
+"""a=int(input())
+if a%3==0 and a%5==0:
+    print("buzz","fizz")
+else:
+  
+  print("buzz")"""
+
+"""name=str(input())
+marks=int(input())
+if marks>90:
+    print(f"name:{name},marks:{marks},grade:A")
+elif marks>80:
+    print(f"name:{name},marks:{marks},grade:B")
+elif marks>70:
+    print(f"name:{name},marks:{marks},grade:C")
+else:
+    print("fail")"""
+
+name=input()
+marks=int(input())
+if marks>=90:
+    grade="A"
+elif marks>=80:
+    grade="B"
+elif marks>=70:
+    grade="C"
+elif marks>=60:
+    grade="D"
+else:
+    grade="fail"
+print(f"name:{name},marks:{marks},grade:{grade}")
+
+
+"""name=input()
+marks=int(input())
+price=5000
+if grade=="A":
+    print("free")
+elif grade=="B":
+    discount=20/100
+elif grade=="C":
+    discount=15/100
+elif grade=="D":
+    discount=10/100
+amount=price-(price*discount)"""
+
+"""x = int(input())
+if 10>x:
+    print("True")
+else:
+    print("False")"""
+
+
+
+    
+
