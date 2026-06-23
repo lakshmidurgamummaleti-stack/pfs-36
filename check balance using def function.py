fa8b4f5b875ff3aa91a7b4b31066cd7af23284bb
lakshmidurgamummaleti-stack <lakshmidurgamummaleti@gmail.com>
2026-06-23 16:48:52 +0530

    Update check balance using def function.py

--- check balance using def function.py
+++ check balance using def function.py
@@ -19,20 +19,24 @@ def deposit():
     print("Current balance:", balance)
 def atm():
     while True:
-        print("\n1. Check Balance")
-        print("2. Withdraw")
-        print("3. Deposit")
-        print("4. Exit")
-        choice = int(input("Enter your choice: "))
-        if choice == 1:
-            check_balance()
-        elif choice == 2:
-            withdraw()
-        elif choice == 3:
-            deposit()
-        elif choice == 4:
-            print("Thank you, visit again!")
-            break
-        else:
-            print("Invalid choice")
+        count=1
+        while count<=3:
+            pin=int(input("enter pin:"))
+        if pin==1234:
+            print("\n1. Check Balance")
+            print("2. Withdraw")
+            print("3. Deposit")
+            print("4. Exit")
+            choice = int(input("Enter your choice: "))
+            if choice == 1:
+                check_balance()
+            elif choice == 2:
+                withdraw()
+            elif choice == 3:
+                deposit()
+            elif choice == 4:
+                print("Thank you, visit again!")
+                break
+            else:
+                print("Invalid choice")
 atm()
