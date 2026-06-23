a430cd36848de1302bdaaeccaf77b51fb90c628f
lakshmidurgamummaleti-stack <lakshmidurgamummaleti@gmail.com>
2026-06-23 14:07:52 +0530

    Add files via upload

---
+++ rapido.py
@@ -0,0 +1,58 @@
+choice=int(input("enter your choice"))
+"""if choice==1:
+    print("bike")
+    km=int(input())
+    if km in range(1,9):
+        price=km*5
+        print("total price",price)
+    elif km in range(9,16):
+        price=(km-8)*6+40
+        print("total price",price)
+    elif km in range(16,26):
+        price=(km-15)*8+76
+        print("total price",price)
+    elif km in range(26,31):
+        price(km-25)*10+188
+        print("total price",price)
+    else:
+        print("ride is not available")"""
+"""if choice==2:
+    print("Auto")
+    km=int(input())
+    if km in range(1,11):
+        price=km*8   #1–10 km → 10 × 8 = ₹80
+        print("Total price:",price)
+    elif km in range(11,20):
+        price=(km-10)*10+80  #11–19 km → 9 × 10 = ₹90
+        print("Total price:",price)
+    elif km in range(20,36):
+        price=(km-19)*12+170   #20–35 km → 16 × 12 = ₹192
+        print("Total price:",price)
+    elif km in range(36,61):
+        price=(km-35)*15+362   #36–40 km → 5 × 15 = ₹75
+        print("Total price:",price)
+    else:
+        print("No ride available")"""
+"""if choice==3:
+    print("Car")
+    km=int(input())
+    if km in range(1,51):
+        price=km*12
+        print("Total price:",price)
+    elif km in range(51,81):
+        price=(km-80)*15+588
+        print("Total price:",price)
+    elif km in range(81,121):
+        toll=int(input("enter toll amount:"))
+        price=(km-120)*17+1023+toll
+        print("Total price:",price)
+    elif km in range(121,201):
+        toll=int(input("enter toll amount:"))
+        food=int(input("enter food bill:"))
+        price=(km-200)*18+food+2274+toll
+        print("Total price:",price)
+    else:
+        print("No ride is available")"""    
+
+    
+    
