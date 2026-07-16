0757c338299b63d26cb3c6dcc6489ac8f24848b9
Mummaleti Lakshmi Durga <lakshmidurgamummaleti@gmail.com>
2026-07-16 10:14:12 +0530

    Add files via upload

---
+++ outputs.py
@@ -0,0 +1,41 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+========================= RESTART: C:\Users\HP\list.py =========================
+[1, 'coder', True, (2+3j), 12.234]
+1 (2+3j)
+12.234
+[1, 'coder', True]
+[1, 'coder', True, (2+3j), 12.234, 'lakshmi']
+[1, 'coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani']
+[1, 'coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani', ['lakshmi', 'kalyani']]
+[1, 'vasu', 'coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani', ['lakshmi', 'kalyani']]
+['vasu', 'coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani', ['lakshmi', 'kalyani']]
+['coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani', ['lakshmi', 'kalyani']]
+16
+1
+6
+5
+1352015474240
+>>> 
+========================= RESTART: C:\Users\HP\list.py =========================
+[1, 'coder', True, (2+3j), 12.234]
+1 (2+3j)
+12.234
+[1, 'coder', True]
+[1, 'coder', True, (2+3j), 12.234, 'lakshmi']
+[1, 'coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani']
+[1, 'coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani', ['lakshmi', 'kalyani']]
+[1, 'vasu', 'coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani', ['lakshmi', 'kalyani']]
+['vasu', 'coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani', ['lakshmi', 'kalyani']]
+['coder', True, (2+3j), 12.234, 'lakshmi', 'lakshmi', 'kalyani', ['lakshmi', 'kalyani']]
+16
+1
+6
+5
+2021184630336
+2021184633984
+>>> 
+========================= RESTART: C:/Users/HP/day6.py =========================
+(1, 2, 3)
+7
