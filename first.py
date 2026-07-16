319f55a27989dd5b7c85fb9689d1b31ff9c102fa
Mummaleti Lakshmi Durga <lakshmidurgamummaleti@gmail.com>
2026-07-16 10:07:14 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,18 @@
+"""help("modules")#display the all modules"""
+import math
+print(help(math))
+print(dir(math))
+x=1.23
+print(math.ceil(x))
+print(math.floor(x))
+print(math.sqrt(25))
+print(math.remainder(20,120))
+
+print(math.radians(30))#convert the degree to radians
+print(math.sin(30))
+print(math.sinh(30))
+print(math.pow(10,2))
+print(math.log(1))
+print(math.lcm(10,20,30))
+print(math.factorial(5))
+print(math.fabs(-12354.33456))
